dd18fe61e9bc9bf47e66ba5a300716318d1bbf4a
Carolina Mascarenhas <carolina.mascarenhass96@hotmail.com>
2022-06-06 10:13:48 -0300

    arrumando req 3 para passar junto com o 7

--- middlewares/salesValidation.js
+++ middlewares/salesValidation.js
@@ -1,22 +1,24 @@
 const Joi = require('joi');
 
-const schema = Joi.object({
+const schema = Joi.array().items({
   productId: Joi.number().required(),
   quantity: Joi.number().min(1).required(),
-});
+}).required();
 
 const validation = (req, res, next) => {
-  req.body.forEach((obj) => {
-    const { error } = schema.validate(obj);
+  const { error } = schema.validate(req.body);
 
-    if (!error) return next();
+  if (!error) return next();
 
-    const messages = error.details.map((e) => e.message);
+  const messages = error.details.map((e) => e.message);
+  // console.log('console do array', messages);
 
-    if (messages[0].includes('require')) return res.status(400).json({ message: messages[0] });
+  const messageReturned = messages[0].slice(0, 1).concat(messages[0].slice(5));
+  // console.log('console da mensagem:', messageReturned);
 
-    res.status(422).json({ message: messages[0] });
-  });
+  if (messages[0].includes('require')) return res.status(400).json({ message: messageReturned });
+
+  res.status(422).json({ message: messageReturned });
 };
 
 module.exports = validation;
