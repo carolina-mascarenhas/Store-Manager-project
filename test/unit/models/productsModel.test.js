a75a0f5448c4cfdd864841ebdc08b5bd4e7e18f0
Carolina Mascarenhas <carolina.mascarenhass96@hotmail.com>
2022-06-09 09:58:42 -0300

    testes getAll products model

--- test/unit/models/productsModel.test.js
+++ test/unit/models/productsModel.test.js
@@ -39,3 +39,82 @@ describe('Insere um novo produto no BD', () => {
 
   });
 });
+
+describe('Busca todos os produtos no BD', () => {
+
+  describe('quando não existe nenhum produto cadastrado', () => {
+
+    before(() => {
+      const execute = [[]];
+      sinon.stub(connection, 'execute').resolves(execute);
+    });
+  
+    after(() => {
+      connection.execute.restore();
+    });
+
+    it('retorna um array', async () => {
+      const [result] = await productsModel.getAll();
+      expect(result).to.be.an('array');
+    });
+
+    it('o array está vazio', async () => {
+      const [result] = await productsModel.getAll();
+      expect(result).to.be.empty;
+    });
+
+  });
+
+  describe('quando existem produtos cadastrados', () => {
+
+    before(() => {
+      const execute = [[
+        {
+          id: 1,
+          name: "Martelo de Thor",
+          quantity: 10
+        },
+        {
+          id: 2,
+          name: "Traje de encolhimento",
+          quantity: 20
+        },
+        {
+          id: 3,
+          name: "Escudo do Capitão América",
+          quantity: 30
+        }
+      ]];
+      sinon.stub(connection, 'execute').resolves(execute);
+    });
+  
+    after(() => {
+      connection.execute.restore();
+    });
+
+    it('retorna um array', async () => {
+      const [result] = await productsModel.getAll();
+      expect(result).to.be.an('array');
+    });
+
+    it('o array não está vazio', async () => {
+      const [result] = await productsModel.getAll();
+      expect(result).not.to.be.empty;
+    });
+
+    it('o array possui itens do tipo objeto', async () => {
+      const [result] = await productsModel.getAll();
+      expect(result[0]).to.be.an('object');
+      expect(result[1]).to.be.an('object');
+      expect(result[2]).to.be.an('object');
+    });
+
+    it('tais itens possuem as propriedades: "id", "name" e "quantity"', async () => {
+      const [result] = await productsModel.getAll();
+      expect(result[0]).to.include.all.keys('id', 'name', 'quantity');
+      expect(result[1]).to.include.all.keys('id', 'name', 'quantity');
+      expect(result[2]).to.include.all.keys('id', 'name', 'quantity');
+    });
+
+  });
+});
